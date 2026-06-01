cad29faa952f29df31f1472f23d7328aff2e78dd
Manzi Allan <manziar12@gmail.com>
2026-06-01 13:57:14 +0200

    dropdown list

--- src/View/Navbar.vue
+++ src/View/Navbar.vue
@@ -9,17 +9,45 @@
       <li><RouterLink to="/about" class="hover:text-yellow-300">About Us</RouterLink></li>
       <li><RouterLink to="/service" class="hover:text-yellow-300">Services</RouterLink></li>
 
-      <li class="relative group">
-        <a href="#" class="hover:text-yellow-300 relative-group">Gallery <span>▼</span></a>
-        <ul class="invisible group-hover:visible opacity-0 group-hover:opacity-100 absolute bg-white text-green-900 rounded shadow w-48 top-2.5 mt-2 z-50 left">
+      <li class="relative inline-flex">
+        <button
+          type="button"
+          class="hover:text-yellow-300 inline-flex items-center gap-1"
+          @mouseenter="activeDropdown = 'gallery'"
+          @mouseleave="closeDropdown"
+        >
+          Gallery <span class="text-xs">▼</span>
+        </button>
+        <ul
+          @mouseenter="activeDropdown = 'gallery'"
+          @mouseleave="closeDropdown"
+          :class="[
+            'transition-all duration-200 absolute bg-white text-green-900 rounded shadow w-48 top-full mt-2 z-50 left-0',
+            activeDropdown === 'gallery' ? 'visible opacity-100' : 'invisible opacity-0'
+          ]"
+        >
           <li class="block px-4 py-2 hover:bg-green-200 text-green-900"><RouterLink to="/photos">Photos</RouterLink></li>
           <li class="block px-4 py-2 hover:bg-green-200 text-green-900"><RouterLink to="/videos">Videos</RouterLink></li>
         </ul>
       </li>
 
-      <li class="relative group">
-        <a href="#" class="hover:text-yellow-300">Destinations<span>▼</span></a>
-        <ul class="invisible group-hover:visible opacity-0 group-hover:opacity-100 absolute bg-white text-green-900 rounded top-2.5 shadow w-48 mt-2 z-50 left">
+      <li class="relative inline-flex">
+        <button
+          type="button"
+          class="hover:text-yellow-300 inline-flex items-center gap-1"
+          @mouseenter="activeDropdown = 'destinations'"
+          @mouseleave="closeDropdown"
+        >
+          Destinations <span class="text-xs">▼</span>
+        </button>
+        <ul
+          @mouseenter="activeDropdown = 'destinations'"
+          @mouseleave="closeDropdown"
+          :class="[
+            'transition-all duration-200 absolute bg-white text-green-900 rounded shadow w-48 top-full mt-2 z-50 left-0',
+            activeDropdown === 'destinations' ? 'visible opacity-100' : 'invisible opacity-0'
+          ]"
+        >
           <li class="block px-4 py-2 hover:bg-green-200 text-green-900"><RouterLink to="/north">Northern Province</RouterLink></li>
           <li class="block px-4 py-2 hover:bg-green-200 text-green-900"><RouterLink to="/south">Southern Province</RouterLink></li>
           <li class="block px-4 py-2 hover:bg-green-200 text-green-900"><RouterLink to="/west">Western Province</RouterLink></li>
@@ -42,5 +70,11 @@
 
 <script setup>
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
 import { locale, toggleLanguage } from '../translation.js';
+
+const activeDropdown = ref(null);
+const closeDropdown = () => {
+  activeDropdown.value = null;
+};
 </script>
